7fcef1e650a18ce20263320ad13953a36902b489
DanyMencu <dany69men@gmail.com>
2022-02-03 15:45:42 +0100

    Create relation between Genre and Book ( one to many)

--- app/Book.php
+++ app/Book.php
@@ -7,11 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     //Mass assignment
-
     protected $fillable = [
         'title',
         'author',
         'slug',
         'content',
     ];
+
+    //Relation with genres
+    public function genre() {
+        return $this->belongsTo('App\Genre');
+    }
 }
--- app/Genre.php
+++ app/Genre.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
-    //
+    //Relation with books
+    public function books() {
+        return $this->hasMany('App\Genre');
+    }
 }
