e6066c62289b212f36b89d134b75abad18e8d740
DanyMencu <dany69men@gmail.com>
2022-02-14 23:05:44 +0100

    Basic send welcome email

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Mail;
+use App\Mail\WelcomeNewMail;
+
 class HomeController extends Controller
 {
     //Admin Homepage
     public function index() {
+
+        //Welcome email
+        Mail::to('fake-account@example.com')->send(new WelcomeNewMail());
 
         return view('admin.home');
     }
